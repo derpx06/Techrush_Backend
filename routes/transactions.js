869ea0b80df67067bb7b0adaf745908a225b2432
Manas Khairnar <manaskhairnar1511@gmail.com>
2025-08-08 00:06:07 +0530

    some form datas

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const transactionController = require('../controllers/transactionController');
+const multer = require('multer')
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-router.post('/send', auth, transactionController.sendPayment);
-router.post('/request', auth, transactionController.requestPayment);
-router.get('/history', auth, transactionController.getTransactionHistory);
+router.post('/send',upload.none(), auth, transactionController.sendPayment);
+router.post('/request',upload.none(), auth, transactionController.requestPayment);
+router.get('/history',auth, transactionController.getTransactionHistory);
 
 
 module.exports = router;
