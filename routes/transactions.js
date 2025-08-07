6610bb484e50df9baf0a6e5f2a34027bc00bc18e
Manas Khairnar <manaskhairnar1511@gmail.com>
2025-08-08 00:17:10 +0530

    recover

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const transactionController = require('../controllers/transactionController');
-const multer = require('multer')
-const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-router.post('/send',upload.none(), auth, transactionController.sendPayment);
-router.post('/request',upload.none(), auth, transactionController.requestPayment);
+
+router.post('/send', auth, transactionController.sendPayment);
+router.post('/request', auth, transactionController.requestPayment);
 router.get('/history',auth, transactionController.getTransactionHistory);
 
 
